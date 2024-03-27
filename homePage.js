62dccd0a1efb9aba2d1fb26e2f27fb0aab1f903a
brainstorm-main <openminderdocs@gmail.com>
2024-03-27 18:03:43 +0100

    Revert "v1"
    This reverts commit b2d88baa2e44143a5ad1459003443e1c07deb0f7.

--- homePage.js
+++
@@ -1,9 +0,0 @@
-let buttons = document.querySelectorAll('.cat');
-for (let but of buttons) {
-    but.addEventListener('click', () => {
-        console.log('pressed');
-
-        window.location.href = `Pages/books.html?categroy=${but.getAttribute('alt')}`;
-
-    });
-}
